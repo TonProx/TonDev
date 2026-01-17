d17f871224945d738f9bbf5863dba93f6b0380bf
TonProx <50390197+TonProx@users.noreply.github.com>
2026-01-17 14:34:30 +0700

    Create Name.py

---
+++ Name.py
@@ -0,0 +1,21 @@
+git init
+
+git config --global user.name "ชื่อภาษาอังกฤษ"
+
+git config --global user.email "อีเมลที่สมัครGitHub"
+
+git add index.html  # เลือกเฉพาะไฟล์ index.html
+
+git add .           # เหมา!  เลือกทุกไฟล์ที่แก้มาได้
+
+git status
+
+git commit - m "ข้อความ"
+
+git branch - m main
+
+git remote add origit enfloenfopwsndpiwlnfpilwnfopilenfopisnfdoi
+
+git remote -vw43q
+
+git push -u origin main
